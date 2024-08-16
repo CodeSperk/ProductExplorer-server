b9d757c912ba0a41c9263301dcb7e7a24fdd53c7
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-08-16 15:28:47 +0600

    set up mongodb and connected

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+//userName: inbxmahbub
+//password: CZjVT1S8UIwyYbQC
+
+//user: productManager
+// password: 8deqaL35zqTgSqQK
+
 
 //middlewares
 app.use(cors({
@@ -10,6 +18,35 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
 }));
 app.use(express.json());
+
+
+const uri = "mongodb+srv://inbxmahbub:CZjVT1S8UIwyYbQC@cluster0.1vrkz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get("/", (req, res) => {
   res.send("welcome to backend server");
