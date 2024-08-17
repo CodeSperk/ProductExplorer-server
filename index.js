a12f1d1ec348a91d529e82bdfb2b9b3adab98daa
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-08-17 15:29:08 +0600

    updated product collection with modified database collection

--- index.js
+++ index.js
@@ -1,59 +1,79 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, Collection } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //middlewares
-app.use(cors({
-  origin: ["http://localhost:5173"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.1vrkz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Collections
     const database = client.db("ProductDB");
-    const productCollection = database.collection("products");
+    const productCollection = database.collection("allProducts");
 
-
-    app.get('/productsCount', async (req, res) => {
+    app.get("/productsCount", async (req, res) => {
       const count = await productCollection.estimatedDocumentCount();
-      res.send({count});
-    })
+      res.send({ count });
+    });
 
     // to get products data from server
-    app.get("/products", async(req, res) => {
-      const searchQuery = req.query.q || "";
+    app.get("/products", async (req, res) => {
+      const {
+        q,
+        brand,
+        category,
+        minPrice,
+        maxPrice,
+        sortBy,
+        page = 0,
+        size = 8,
+      } = req.query;
+
       const query = {
-        productName: { $regex: searchQuery, $options: "i" }
+        ...(q && { productName: { $regex: q, $options: "i" } }),
+        ...(brand && { brandName: brand }),
+        ...(category && { categoryName: category }),
+        ...(minPrice &&
+          maxPrice && {
+            price: { $gte: parseFloat(minPrice), $lte: parseFloat(maxPrice) },
+          }),
       };
 
-      const page = parseInt(req.query.page) || 0;
-      const size = parseInt(req.query.size) || 8;
+      const sortOptions = {
+        ...(sortBy === "priceLowHigh" && { price: 1 }),
+        ...(sortBy === "priceHighLow" && { price: -1 }),
+        ...(sortBy === "dateAdded" && { dateAdded: -1 }),
+      };
 
-      const cursor = productCollection.find(query).skip(page * size).limit(size);
+      const cursor = productCollection.find(query).sort(sortOptions).skip(page * size).limit(parseInt(size));
       const result = await cursor.toArray();
-            
-      res.send(result);
-    })
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -61,13 +81,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
 app.get("/", (req, res) => {
   res.send("welcome to backend server");
-})
+});
 
 app.listen(port, () => {
   console.log("app is running on the port : ", port);
-})
+});
